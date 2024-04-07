2059b150857e2d8ae805cafd0d7c285421cf5bf0
Kim-Onesmus <kimones1122@gmail.com>
2024-04-07 12:39:54 +0300

    git ignore

--- app/views.py
+++ app/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 import requests
 
 # Create your views here.
+API_KEY = 'aeeace275047440788ac8885dd143e23'
+
 def Home(request):
-    url = f''
+    url = 'https://newsapi.org/v2/everything?q=tesla&from=2024-03-07&sortBy=publishedAt&apiKey={API_KEY}'
+    response = requests.get(url)
+    data = response.json()
+    print(data)
